8c46a815122a0a62da395d9624eb68835f4b6335
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2025-01-31 10:57:29 +0100

    tada

---
+++ kicker.ps1
@@ -0,0 +1,27 @@
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Restart the script as admin
+    Write-Host "Requesting administrative privileges..."
+    Start-Process powershell "-File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
+Write-Host "Ready to kick >:)"
+$userName = Read-Host "Choose the victim's username"
+$userQuery = (query user | findstr "$userName")
+if (!($userQuery)) {
+    Write-Host "'$userName' not found :("
+} else {
+    Write-Host "$userQuery"
+    Write-Host "'$userName' found >:)"
+    $hostName = (hostname)
+    if (!($hostName)) {
+        Write-Host "Can't get hostname :("
+    } else {
+        Write-Host "Hostname '$hostName' got >:)"
+        $sessionId = ($userQuery -split '\s+')[2]
+        Write-Host "KICKING >:)"
+        logoff $sessionId /server:$hostName
+    }
+}
+Write-Host "Bye!"
+Read-Host "Press any key to exit"
