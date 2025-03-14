a9fc1ae6eee6122e58269b7aa036edbcc6693699
villers <krisztian@villers.hu>
2025-03-14 13:10:52 +0100

    functional formating steps

--- adman.ps1
+++ adman.ps1
@@ -1,8 +1,8 @@
-﻿If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Requesting administrative privileges..."
-    Start-Process powershell "-File `"$PSCommandPath`"" -Verb RunAs
-    Exit
-}
+﻿#If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+#    Write-Host "Requesting administrative privileges..."
+#    Start-Process powershell "-File `"$PSCommandPath`"" -Verb RunAs
+#    Exit
+#}
 
 # Hide PowerShell Console
 Add-Type -Name Window -Namespace Console -MemberDefinition '
@@ -14,12 +14,40 @@ public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 $consolePtr = [Console.Window]::GetConsoleWindow()
 [Console.Window]::ShowWindow($consolePtr, 0)
 
+# Config
+# Form
+# Form size
+$formSizeWidth = 320
+$formSizeHeight = 360
+# Label
+# Label default location
+$global:labelDefaultLocationX = 20
+$global:labelDefaultLocationY = 40
+# Textbox
+# Textbox size
+$global:textboxSizeWidth = 105
+$global:textboxSizeHeight = 20
+# Textbox default location
+$global:textBoxDefaultLocationX = 20
+$global:textBoxDefaultLocationY = 60
+# Button
+# Button size
+$global:buttonSizeWidth = 95
+$global:buttonSizeHeight = 20
+# Button default location
+$global:buttonDefaultLocationX = 150
+$global:buttonDefaultLocationY = 60
+
+# Location increaser
+$global:xIncreaser = 55
+$global:count = 0
+
 # Main Form
 Add-Type -AssemblyName System.Windows.Forms
 $mainForm = New-Object System.Windows.Forms.Form
 $mainForm.Text = "AdMan"
-$mainForm.Width = 300
-$mainForm.Height = 300
+$mainForm.Width = $formSizeWidth
+$mainForm.Height = $formSizeHeight
 $mainForm.StartPosition = "CenterScreen"
 
 # Header Label
@@ -31,39 +59,96 @@ $headerLabel.ForeColor = "White"
 $headerLabel.BackColor = "Black"
 $mainForm.Controls.Add($headerLabel)
 
+function Add-Label {
+    param (
+        [string]$text
+    )
+    $header = New-Object System.Windows.Forms.Label
+    $header.Text = $text
+    $header.AutoSize = $true
+    $x = ($global:labelDefaultLocationX + ($global:count * $global:xIncreaser))
+    $y = $global:labelDefaultLocationY
+    $header.Location = New-Object System.Drawing.Point($x, $y)
+    return $header
+}
+
+function Add-Textbox {
+    param (
+        [string]$placeholder
+    )
+    $textbox = New-Object System.Windows.Forms.TextBox
+    $textbox.ForeColor = "Gray"
+    $textbox.Text = $placeholder
+    $x = $global:textBoxDefaultLocationX + ($global:count * $global:xIncreaser)
+    $y = $global:textBoxDefaultLocationY
+    $textbox.Location = New-Object System.Drawing.Point($x, $y)
+    $textbox.Size = New-Object System.Drawing.Size($global:textboxSizeWidth, $global:textboxSizeHeight)
+    return $textbox
+}
+
+function TextBox-OnFocus {
+    param (
+        [System.Windows.Forms.TextBox]$textbox,
+        [string]$placeholder
+    )
+    if ($textbox.Text -eq $placeholder) {
+        $textbox.Text = ""
+        $textbox.ForeColor = "Black"
+    }
+}
+
+function TextBox-LostFocus {
+    param (
+        [System.Windows.Forms.TextBox]$textbox,
+        [string]$placeholder
+    )
+    if ($textbox.Text -eq "") {
+        $textbox.Text = $placeholder
+        $textbox.ForeColor = "Gray"
+    }
+}
+
+function TextBox-KeyPress {
+    param (
+        [System.Windows.Forms.TextBox]$textbox,
+        [string]$key,
+        [scriptblock]$action
+    )
+    if ($_.KeyCode -eq "Enter") {
+        Write-Host "Enter key pressed"
+        & $action
+    }
+}
+
+function Add-Button {
+    param (
+        [string]$text
+    )
+    $button = New-Object System.Windows.Forms.Button
+    $button.Text = $text
+    $x = $global:buttonDefaultLocationX + ($global:count * $global:xIncreaser)
+    $y = $global:buttonDefaultLocationY
+    $button.Location = New-Object System.Drawing.Point($x, $y)
+    $button.Size = New-Object System.Drawing.Size($global:buttonSizeWidth, $global:buttonSizeHeight)
+    return $button
+}
+
 # Kicker Header
-$kickerLabel = New-Object System.Windows.Forms.Label
-$kickerLabel.Text = "Kick by username:"
-$kickerLabel.AutoSize = $true
-$kickerLabel.Location = New-Object System.Drawing.Point(20, 40)
+$kickerLabel = Add-Label -text "Kick user by username:"
 $mainForm.Controls.Add($kickerLabel)
 
 # Kicker Textbox
 $kickerTextBoxPlaceholder = "username"
-$kickerTextBox = New-Object System.Windows.Forms.TextBox
-$kickerTextBox.ForeColor = "Gray"
-$kickerTextBox.Text = $kickerTextBoxPlaceholder
-$kickerTextBox.Location = New-Object System.Drawing.Point(20, 60)
-$kickerTextBox.Size = New-Object System.Drawing.Size(105, 20)
+$kickerTextBox = Add-Textbox -placeholder $kickerTextBoxPlaceholder
 $mainForm.Controls.Add($kickerTextBox)
-$kickerTextBox.Add_GotFocus({
-    if ($kickerTextBox.Text -eq $kickerTextBoxPlaceholder) {
-        $kickerTextBox.Text = ""
-        $kickerTextBox.ForeColor = "Black"
-    }
-})
-$kickerTextBox.Add_LostFocus({
-    if ($kickerTextBox.Text -eq "") {
-        $kickerTextBox.Text = $kickerTextBoxPlaceholder
-        $kickerTextBox.ForeColor = "Gray"
-    }
-})
+$kickerTextBox.Add_GotFocus({TextBox-OnFocus -textbox $kickerTextBox -placeholder $kickerTextBoxPlaceholder})
+$kickerTextBox.Add_LostFocus({TextBox-LostFocus -textbox $kickerTextBox -placeholder $kickerTextBoxPlaceholder})
 
 # Kicker Button
 $kickerButton = New-Object System.Windows.Forms.Button
 $kickerButton.Text = "Kick"
 $kickerButton.Location = New-Object System.Drawing.Point(150, 60)
-$kickerButton.Size = New-Object System.Drawing.Size(85, 20)
+$kickerButton.Size = New-Object System.Drawing.Size($buttonSizeWidth, $buttonSizeHeight)
 $mainForm.Controls.Add($kickerButton)
 
 # Create a horizontal line using a Label
@@ -88,7 +173,7 @@ $hostnameLookupTextbox = New-Object System.Windows.Forms.TextBox
 $hostnameLookupTextbox.ForeColor = "Gray"
 $hostnameLookupTextbox.Text = $hostnameLookupTextboxPlaceholder
 $hostnameLookupTextbox.Location = New-Object System.Drawing.Point(20, 115)
-$hostnameLookupTextbox.Size = New-Object System.Drawing.Size(105, 20)
+$hostnameLookupTextbox.Size = New-Object System.Drawing.Size($textboxSizeWidth, $textboxSizeHeight)
 $mainForm.Controls.Add($hostnameLookupTextbox)
 $hostnameLookupTextbox.Add_GotFocus({
     if ($hostnameLookupTextbox.Text -eq $hostnameLookupTextboxPlaceholder) {
@@ -107,7 +192,7 @@ $hostnameLookupTextbox.Add_LostFocus({
 $hostnameLookupButton = New-Object System.Windows.Forms.Button
 $hostnameLookupButton.Text = "Get hostname"
 $hostnameLookupButton.Location = New-Object System.Drawing.Point(150, 115)
-$hostnameLookupButton.Size = New-Object System.Drawing.Size(85, 20)
+$hostnameLookupButton.Size = New-Object System.Drawing.Size($buttonSizeWidth, $buttonSizeHeight)
 $mainForm.Controls.Add($hostnameLookupButton)
 
 # Create a horizontal line using a Label
@@ -119,11 +204,46 @@ $line.Height = 2
 $line.Location = New-Object System.Drawing.Point(0, 140)
 $mainForm.Controls.Add($line)
 
+# Get process id for port header
+$hostnameLookupLabel = New-Object System.Windows.Forms.Label
+$hostnameLookupLabel.Text = "Get process ID for port:"
+$hostnameLookupLabel.AutoSize = $true
+$hostnameLookupLabel.Location = New-Object System.Drawing.Point(20, 145)
+$mainForm.Controls.Add($hostnameLookupLabel)
+
+# Get process id for port textbox
+$portProcessTextboxPlaceholder = "port"
+$portProcessTextbox = New-Object System.Windows.Forms.TextBox
+$portProcessTextbox.ForeColor = "Gray"
+$portProcessTextbox.Text = $portProcessTextboxPlaceholder
+$portProcessTextbox.Location = New-Object System.Drawing.Point(20, 165)
+$portProcessTextbox.Size = New-Object System.Drawing.Size($textboxSizeWidth, $textboxSizeHeight)
+$mainForm.Controls.Add($portProcessTextbox)
+$portProcessTextbox.Add_GotFocus({
+    if ($portProcessTextbox.Text -eq $portProcessTextboxPlaceholder) {
+        $portProcessTextbox.Text = ""
+        $portProcessTextbox.ForeColor = "Black"
+    }
+})
+$portProcessTextbox.Add_LostFocus({
+    if ($portProcessTextbox.Text -eq "") {
+        $portProcessTextbox.Text = $portProcessTextboxPlaceholder
+        $portProcessTextbox.ForeColor = "Gray"
+    }
+})
+
+# Get process id for port button
+$portProcessButton = New-Object System.Windows.Forms.Button
+$portProcessButton.Text = "Get process ID"
+$portProcessButton.Location = New-Object System.Drawing.Point(150, 165)
+$portProcessButton.Size = New-Object System.Drawing.Size($buttonSizeWidth, $buttonSizeHeight)
+$mainForm.Controls.Add($portProcessButton)
+
 # Info Label
 $infoLabel = New-Object System.Windows.Forms.Label
 $infoLabel.Text = ""
-$infoLabel.Size = New-Object System.Drawing.Size(240, 40)
-$infoLabel.Location = New-Object System.Drawing.Point(20, 200)
+$infoLabel.Size = New-Object System.Drawing.Size(260, 40)
+$infoLabel.Location = New-Object System.Drawing.Point(20, 260)
 $infoLabel.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle
 $mainForm.Controls.Add($infoLabel)
 
@@ -172,18 +292,14 @@ function userKicker {
         }
     }
 }
+$kickerTextBox.Add_KeyDown({TextBox-KeyPress -textbox $kickerTextBox -key "Enter" -action ${Function:UserKicker}})
 
 # Kicker Button Add Click
 $kickerButton.Add_Click({
     userKicker
 })
 
-# Kicker Textbox Enter
-$kickerTextBox.Add_KeyDown({
-    if ($_.KeyCode -eq "Enter") {
-        userKicker
-    }
-})
+# here
 
 # Hostname Lookup Function
 function hostnameLookup {
@@ -237,6 +353,8 @@ $hostnameLookupTextbox.Add_KeyDown({
         hostnameLookup
     }
 })
+
+# Check process id for port
 
 # Icon
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
